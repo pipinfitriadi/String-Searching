ef54e46611a88a16f27a2ecf72dbbf53e9375fd9
Pipin Fitriadi <pipinfitriadi@gmail.com>
2019-05-23 10:17:23 +0700

    Mempersingkat proses kodingan Hackerrank's Problem. Algoritma Aho-Carosick belum diimpelementasikan.

--- hackerrank_determining_dna_health.py
+++ hackerrank_determining_dna_health.py
@@ -4,22 +4,32 @@
 Hackerrank: Determining DNA Health
 URL: https://www.hackerrank.com/challenges/determining-dna-health/problem
 
-Try to be solved by Pipin Fitriadi (pipinfitriadi@gmail.com) at May 21th 2019.
+Try to be solved by Pipin Fitriadi (pipinfitriadi@gmail.com) at May 21th 2019,
+updated at May 23th 2019.
 """
 
+from collections import defaultdict
+
 if __name__ == '__main__':
-    with open('input30.txt', 'r') as f:
+    with open('../input0.txt', 'r') as f:
         test_case = f.readlines()
 
-    genes = test_case[1].rstrip().split()
-    health = list(
+    genes = list(
         map(
-            int,
-            test_case[2].rstrip().split()
+            tuple,
+            zip(
+                test_case[1].rstrip().split(),
+                list(
+                    map(
+                        int,
+                        test_case[2].rstrip().split()
+                    )
+                )
+            )
         )
     )
-    min_health = -1
-    max_health = -1
+    # min_health = -1
+    # max_health = -1
 
     for s_itr in range(
         int(
@@ -34,31 +44,36 @@ if __name__ == '__main__':
             first_last_d[1]
         )
         d = first_last_d[2]
-        total_health = 0
-        gene = genes[first:last+1]
+    #     total_health = 0
+        dna_gene = defaultdict(list)
 
-        for g in set(gene):
-            gene_health = 0
+        for gene, health in genes[first:last + 1]:
+            dna_gene[gene].append(health)
 
-            for k, v in enumerate(gene):
-                if v == g:
-                    gene_health += health[k+first]
+        dna_gene = sorted(dna_gene.items())
 
-            found = 0
-            dummy_d = d[found:]
+    #     for g in set(dna_gene):
+    #         gene_health = 0
 
-            while found != -1 and found < len(dummy_d):
-                found = dummy_d.find(g)
+    #         for k, v in enumerate(gene):
+    #             if v == g:
+    #                 gene_health += health[k+first]
 
-                if found != -1:
-                    total_health += gene_health
-                    found += 1
-                    dummy_d = dummy_d[found:]
+    #         found = 0
+    #         dummy_d = d[found:]
 
-        if min_health == -1 or total_health < min_health:
-            min_health = total_health
+    #         while found != -1 and found < len(dummy_d):
+    #             found = dummy_d.find(g)
 
-        if max_health == -1 or total_health > max_health:
-            max_health = total_health
+    #             if found != -1:
+    #                 total_health += gene_health
+    #                 found += 1
+    #                 dummy_d = dummy_d[found:]
 
-    print(min_health, max_health)
+    #     if min_health == -1 or total_health < min_health:
+    #         min_health = total_health
+
+    #     if max_health == -1 or total_health > max_health:
+    #         max_health = total_health
+
+    # print(min_health, max_health)
